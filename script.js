6836ab0dd729bb56497117590050c87c19c64839
Hacker3846 <ibakar384@gmail.com>
2026-06-05 01:44:47 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
- function c() {
-        const g = id => document.getElementById(id), v = id => g(id).value;
-        const p = +v('p')||0, l = +v('l')||0, w = +v('w')||0, hStr = v('h').trim();
-        const isV = hStr !== "" && !isNaN(+hStr), res = isV ? l * w * (+hStr) : l * w;
-        g('title').innerText = isV ? "Расчет объема" : "Расчет площади";
-        g('lbl').innerText = isV ? "Итоговый объем:" : "Итоговая площадь:";
-        g('rf').innerText = +res.toFixed(4);
-        g('rm').innerText = (res * p).toLocaleString('ru-RU') + " руб.";
-    }
